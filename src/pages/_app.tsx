f90efc43932e0ad075c89a64caa073772826470a
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-02-28 18:03:04 -0300

    Updates app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
-import theme from '../styles/theme/light';
+import theme from 'styles/theme/light';
 import GlobalStyle from '../styles/global';
 
-const App = ({ Component, pageProps }: AppProps) =>  (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </ThemeProvider>
-)
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {/* <ToggleTheme toggleTheme={toggleTheme}/> */}
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
 
 export default App;
