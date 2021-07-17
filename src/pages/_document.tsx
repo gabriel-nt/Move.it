9a471d6470e30546a66a0c080f024c340fd20fc2
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-07-17 10:59:31 -0300

    Styled-components ssr added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,38 @@
-import DocumentNext, { Html, Head, Main, NextScript } from 'next/document';
+import DocumentNext, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class Document extends DocumentNext {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await DocumentNext.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html>
